e3455287fcd05934a2a979a9db3958318c6f4f87
Esa Harju <liquiddragon@users.noreply.github.com>
2018-04-08 17:43:24 +0300

    DQ demo app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Designing interactive systems course project
+
+Join the Team!
+
+## Design critique demo
+
+Demo is located under ```dqdemo``` directory and runs on local host only.
+
+Remember to
+* install dependencies ```npm install```
+* set paths properly on **app.js**.
+
---
+++ dqdemo/app.js
@@ -0,0 +1,56 @@
+var express = require('express')
+var app = express()
+// var qrimage = require('qr-image')
+
+// Server port
+const port = 8000
+// Image files location
+var pathToImages = '/path/to/images' // Correct this!
+// Application base URL address
+var baseURL = 'http://localhost:8000'
+
+// Available teams
+var availTeams = ['blue', 'green', 'red', 'yellow', 'aquamarine', 'blueviolet', 'brown', 'gold']
+// Created teams and their member count
+var existingTeams = {}
+
+// Express.JS settings
+app.set('view engine', 'pug')
+app.use(express.static('/'))
+
+// Root page, i.e. where it all begins
+app.get('/', function (req, res) {
+  res.render('index', {new_team_qr_code: pathToImages + 'qr-helsinki-fi.svg'})
+})
+
+// Temporary URL for team creation that redirects to team home page
+app.get('/newteam', function (req, res) {
+  var newTeam = availTeams.pop()
+  existingTeams[newTeam] = 1
+
+  res.redirect('/team/' + newTeam)
+})
+
+// New team home page for team leader and members
+app.get('/team/:team', function (req, res) {
+  var teamSize = existingTeams[req.params.team]
+  var teamPage = 'Welcome to ' + req.params.team + ' team!'
+  var teamColor = req.params.team
+  var newTeamMemberLink = baseURL + '/team/' + req.params.team + '/newmember'
+  var teamQrCode = pathToImages + 'qr-helsinki-fi.svg'
+  var teamInfo = 'Your team has ' + teamSize + ' ' + (teamSize === 1 ? 'member' : 'members')
+
+  res.render('team_page', {team_page: teamPage, team_color: teamColor, new_team_member: newTeamMemberLink, team_qr_code: teamQrCode, team_info: teamInfo})
+})
+
+// Temporary URL for team members to join a team
+app.get('/team/:team/newmember', function (req, res) {
+  existingTeams[req.params.team] += 1
+
+  res.redirect('/team/' + req.params.team)
+})
+
+// Create server
+app.listen(port, () => {
+  console.log('Listening on ' + port)
+})
