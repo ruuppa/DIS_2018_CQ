dd52788552dd692f7cada31f6ff7430d9a7effd0
ruuppa <ruuppa@users.noreply.github.com>
2018-04-25 23:42:42 +0300

    Add files via upload
    -Changed to work with QR-image

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ var config = {}
 
 // App configuration
 config.settings = {}
-config.settings.pathToImages = '/path/to/images/' // Fix this!
+//config.settings.pathToImages = '/path/to/images/' // Fix this!
+config.settings.pathToImages = '/School/School5/DIS/Project/DIS_2018_CQ-master/images/'
 config.settings.port = process.env.WEB_PORT || 8000
 config.settings.baseURL = 'http://localhost:' + config.settings.port
 
