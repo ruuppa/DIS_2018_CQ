c2d8ed83b103e57c00b0c7fdb54b2fbecab9fc65
ruuppa <ruuppa@users.noreply.github.com>
2018-04-28 14:23:55 +0300

    Add files via upload
    -Changed the ranking-counter slightly: Now it shows the player's current rank on their own page
    -Cleaned code a bit and altered the style

--- api/routes.js
+++ api/routes.js
@@ -2,7 +2,7 @@ module.exports = function (app, config) {
   var qrimage = require('qr-image')
 // Root page, i.e. where it all begins
   app.get('/', function (req, res) {
-	res.render('index', {new_team_qr_code: createQRLink(config.settings.baseURL + '/newteam', 200, 200),teams: config.existingTeams})
+	res.render('index', {new_team_qr_code: createQRLink(config.settings.baseURL + '/newteam', 200, 200),teams: config.existingTeams, keys_sorted: sortKeys(config.existingTeams)})
   })
 
   // Temporary URL for team creation that redirects to team home page
@@ -15,6 +15,8 @@ module.exports = function (app, config) {
 
   // New team home page for team leader and members
   app.get('/team/:team', function (req, res) {
+	var currentRank = sortKeys(config.existingTeams).indexOf(req.params.team)+1
+	
     var teamSize = config.existingTeams[req.params.team]
     var teamPage = 'Welcome to ' + req.params.team + ' team!'
     var teamColor = req.params.team
@@ -22,7 +24,8 @@ module.exports = function (app, config) {
 	var teamQrCode = createQRLink(newTeamMemberLink, 200, 200)
     var teamInfo = 'Your team has ' + teamSize + ' ' + (teamSize === 1 ? 'member' : 'members')
     var subTeamInfo = 'You need ' + config.game.maxTeamSize + ' members in your team to win!'
-    var gameStatus = 'Game ongoing'
+    var gameStatus = 'Game ongoing, your rank is: '+currentRank
+	
     if (teamSize >= config.game.maxTeamSize) {
       gameStatus = 'You made it!'
       config.game.inprogress = true
@@ -48,5 +51,10 @@ module.exports = function (app, config) {
   var fixedSvg = svgStr.substr(0, idx) + ' width="' + width + '" height="' + height + '"' + svgStr.substr(idx)
 
   return fixedSvg
+  }
+  //Sort the ranking of connections
+  function sortKeys (teams) {
+   var keysSorted = Object.keys(teams).sort(function(a,b){return teams[b]-teams[a]})
+   return keysSorted
   }
 }
